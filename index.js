903202149445a39b378aeb409407380656c089eb
Nicholas Tate <ntate22@gmail.com>
2016-12-31 21:27:56 -0500

    added plugin hook for cli

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ const yargs = require('yargs');
 class ContainershipCli {
     constructor(options) {
         this.options = options || {};
-        this.options.pluginPath = path.normalize(this.options.pluginPath || constants.DEFAULT_PLUGIN_PATH);
+        this.options.pluginPath = path.normalize(expandHome(this.options.pluginPath || constants.DEFAULT_PLUGIN_PATH));
+        this.options.pluginModulePath = `${this.options.pluginPath}/node_modules`;
 
         this.options.cliConfigPath = path.normalize(this.options.cliConfigPath || constants.DEFAULT_CLI_CONFIG_PATH);
         configuration.initialize(this.options.cliConfigPath, this.options.pluginPath);
 
         this.commands = fs.readdirSync(path.normalize('./commands')).map(file => file.substring(0, file.indexOf('.js')));
-        this.plugins = fs.existsSync(this.options.pluginPath) ? fs.readdirSync(this.options.pluginPath) : [];
+        this.plugins = fs.existsSync(this.options.pluginModulePath) ? fs.readdirSync(this.options.pluginModulePath) : [];
     }
 
     run() {
@@ -30,7 +31,20 @@ class ContainershipCli {
             return yargs.command(require(`./commands/${cmd}`)).argv;
         }
 
-        // todo - check if it is command of an installed plugin, load, and execute
+        this.plugins.forEach((name) => {
+            const req = require(`${this.options.pluginModulePath}/${name}`);
+            const plugin = typeof req === 'function' ? new req() : req;
+
+            if (plugin.version === 'v2' && plugin.cli !== null) {
+                yargs.command(plugin.cli);
+
+                if (plugin.cli.command === cmd) {
+                    yargs.argv;
+                    process.exit(0);
+                }
+            }
+
+        });
 
         return this.unknownCommand();
     }
@@ -38,10 +52,24 @@ class ContainershipCli {
     unknownCommand() {
         this.commands.forEach(cmd => yargs.command(require(`./commands/${cmd}`)));
 
-        // todo: load all plugin cli commands before showing help
+        // plugins would have already been added as we have to instantiate each one to check in the run command
 
         yargs.showHelp('log');
     }
+}
+
+function expandHome(path) {
+    if (!path) {
+        return path;
+    }
+
+    if (path === '~') {
+        return process.env.HOME;
+    }
+
+    path = path.replace('~/', `${process.env.HOME}/`);
+
+    return path;
 }
 
 module.exports = ContainershipCli;
